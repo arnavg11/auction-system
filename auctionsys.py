71dae071b82b7cf675a9275a476d02aa69599c66
arnav gupta <77843522+arnavg11@users.noreply.github.com>
2022-04-12 11:41:43 +0530

    Update auctionsys.py

--- auctionsys.py
+++ auctionsys.py
@@ -1,18 +1,33 @@
 import mysql.connector as sql
-passw = "4rn4vGU!"
+import random as rand
+passw = "deens"
 auctioned = ["Lionel Messi"]
-def pickAuctionPlayer(filename = "players_fifa22", primary_key = "fullname"):
+def pickAuctionPlayer(filename = "players_fifa22", primary_key = "fullname",ovr = "overall",size = 40):
     do = sql.connect(host = "localhost", user = "root", password = passw, database = "fifadata")
     ci = do.cursor()
-    if len(auctioned)==0:
-        exec = f"select*from {filename} order by overall desc limit 50"
-    else:
-        exec = f"select * from {filename} where "
-        for i in auctioned:
-            exec+=f"not fullname = '{i}' and"
-        exec = exec[:-3]+ "order by overall desc limit 50"
-    ci.execute(exec)
-
-    p = list(ci)
+    ci.execute(f"desc {filename}")
+    datatypes = []
+    for i in ci:
+        datatypes.append(i[0].lower())
     
-pickAuctionPlayer()
+    if len(auctioned)==0:
+        execstr = f"select*from {filename} order by overall desc limit {size}"
+    else:
+        execstr = f"select * from {filename} where "
+        for i in auctioned:
+            execstr+=f"not fullname = '{i}' and"
+        execstr = execstr[:-3]+ f"order by overall desc limit {size}"
+    ci.execute(execstr)
+    p = list(ci)
+    ovrList= []
+    for i in p:
+        ovrList.append(i[datatypes.index(ovr)])
+    pick = rand.random()*sum(ovrList)
+    for i in range(len(ovrList)):
+        if pick-ovrList[i]>=0:
+            pick-=ovrList[i]
+        else:
+            return p[i]
+    return p[-1]
+        
+print(pickAuctionPlayer())
