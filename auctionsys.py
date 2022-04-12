2a1faa2e58493eed96ea5666462a8cea49751bda
arnav gupta <77843522+arnavg11@users.noreply.github.com>
2022-04-12 22:34:46 +0530

    Update auctionsys.py

--- auctionsys.py
+++ auctionsys.py
@@ -1,8 +1,8 @@
 import mysql.connector as sql
 import random as rand
-passw = "deens"
+passw = "4rn4vGU!"
 auctioned = []
-def pickAuctionPlayer(filename = "players_fifa22", primary_key = "fullname",ovr = "overall",size = 40):
+def pickAuctionPlayer(filename = "players_fifa22", primary_key = "fullname",ovr = "overall",size = 50,influence = 2):
     do = sql.connect(host = "localhost", user = "root", password = passw, database = "fifadata")
     ci = do.cursor()
     ci.execute(f"desc {filename}")
@@ -21,15 +21,13 @@ def pickAuctionPlayer(filename = "players_fifa22", primary_key = "fullname",ovr 
     p = list(ci)
     ovrList= []
     for i in p:
-        ovrList.append(i[datatypes.index(ovr)])
+        ovrList.append(i[datatypes.index(ovr)]**2)
     pick = rand.random()*sum(ovrList)
     for i in range(len(ovrList)):
         if pick-ovrList[i]>=0:
             pick-=ovrList[i]
         else:
             auctioned.append(p[i])
-            return p[i]
+            return dict(zip(datatypes, p[i]))
     auctioned.append(p[-1])
-    return p[-1]
-
-print(pickAuctionPlayer())
+    return dict(zip(datatypes, p[-1]))
