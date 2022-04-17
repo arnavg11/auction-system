5c698171754957e090e247437549783897ad036e
arnav gupta <77843522+arnavg11@users.noreply.github.com>
2022-04-17 19:14:01 +0530

    Update auctionsys.py

--- auctionsys.py
+++ auctionsys.py
@@ -31,3 +31,32 @@ def pickAuctionPlayer(filename = "players_fifa22", primary_key = "fullname",ovr 
             return dict(zip(datatypes, p[i]))
     auctioned.append(p[-1])
     return dict(zip(datatypes, p[-1]))
+def team_init(filename = "players_fifa22"):
+    pl = []
+    op = []
+    do = sql.connect(host = "localhost", user = "root", password = passw, database = "fifadata")
+    ci = do.cursor()
+    ci.execute(f"desc {filename}")
+    datatypes = []
+    for i in ci:
+        datatypes.append(i[0].lower())
+    formn = [1,4,3,3]
+    pos = [["GK"],["LB","RB","CB"],["CM","CAM","CDM","LM","RM"],["RW","ST","LW","CF"]]
+    for i in range(4):
+        s = "select * from players_fifa22 where"
+        for p in pos[i]:
+            s+=f" bestposition = '{p}' or"
+        s = s[:-2]+f"order by rand() limit {formn[i]}"
+        ci.execute(s)
+        t=[]
+        for p in ci:
+            t.append(dict(zip(datatypes, p)))
+        pl.append(t)
+    for j in pl:
+        for i in j:
+            print(i["fullname"],i["overall"],end = "\t")
+        print()
+    
+
+    return pl
+team_init()
