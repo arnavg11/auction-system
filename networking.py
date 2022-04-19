fd34179c07713cfd7c6622990c2c7efd321be041
arnav gupta <77843522+arnavg11@users.noreply.github.com>
2022-04-19 22:37:18 +0530

    Update networking.py

--- networking.py
+++ networking.py
@@ -1,12 +1,18 @@
 import socket as sck
 import threading as thr
-def convertih(ipv4 = sck.gethostbyname(sck.gethostname())):
+temp = sck.socket(sck.AF_INET,sck.SOCK_DGRAM)
+temp.connect(("8.8.8.8",9000))
+myip = temp.getsockname()[0]
+
+
+
+def convertih(ipv4 = myip):
     return "".join([hex(int(i))[-2:] for i in ipv4.split(".")])
 def converthi(ip4_e):
     ip = ""
     for i in range(4):
         if ip4_e[i*2]=="x":
-            ip+=str(int("0"+ip4_e[i*2+1],16))[:-2]
+            ip+=str(int(ip4_e[i*2+1],16))
         else:
             ip+=str(int(ip4_e[i*2:i*2+2],16))
         ip+="."
@@ -15,13 +21,14 @@ def converthi(ip4_e):
 class Comp:
     def __init__(self):
         self.socket = sck.socket(sck.AF_INET, sck.SOCK_DGRAM)
-        self.socket.bind((sck.gethostbyname(sck.gethostname()),6000))
+        self.socket.bind((myip,6000))
         self.ip = None
     def read(self):
         return self.socket.recvfrom(4096)
     def write(self,msg):
         print(self.ip, type(msg.encode()))
         self.socket.sendto(msg.encode(),(self.ip,6000))
+        print(1)
     def setip(self,ip,firstms = False):
         self.ip = ip
         if firstms:self.write(f"JOIN: {convertih()}")
